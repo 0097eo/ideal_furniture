aae96c7c775ce75d4dcd1c315fa12e3a0fa51e64
0097eo <emmanuelokello294@gmail.com>
2024-09-03 11:54:45 +0300

    add style to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../utils/AuthContext';
+import '../App.css'; 
 
 const Navbar = () => {
   const { user, logout } = useAuth();
@@ -12,32 +13,37 @@ const Navbar = () => {
   };
 
   return (
-    <nav style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 20px' }}>
-      <div>
-        <ul style={{ listStyleType: 'none', margin: 0, padding: 0 }}>
+    <nav>
+      <div className="nav-logo">
+        <span className="furniture">Ideal Furniture</span> & <span className="decor">Decor</span>
+      </div>
+      <div className="nav-center">
+        <ul>
           <li>
-            <Link to="/products" style={{ textDecoration: 'none', color: 'inherit' }}>Browse</Link>
+            <Link to="/products">Browse</Link>
           </li>
+          {user && (
+            <>
+              <li>
+                <Link to="/cart">Cart</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
-      <div>
+      <div className="nav-links">
         {!user ? (
           <>
-            <Link to="/signup" style={{ marginRight: '10px', textDecoration: 'none', color: 'inherit' }}>Signup</Link>
-            <Link to="/login" style={{ textDecoration: 'none', color: 'inherit' }}>Login</Link>
+            <Link to="/signup">Signup</Link>
+            <Link to="/login">Login</Link>
           </>
         ) : (
-          <>
-            <Link to="/cart" style={{ textDecoration: 'none', color: 'inherit' }}>Cart</Link>
-            <Link to="/dashboard" style={{ marginRight: '10px', textDecoration: 'none', color: 'inherit' }}>Dashboard</Link>
-            <button 
-              onClick={handleLogout} 
-              style={{ background: 'none', border: 'none', color: 'inherit', cursor: 'pointer' }}
-              aria-label="Logout"
-            >
-              Logout
-            </button>
-          </>
+          <button onClick={handleLogout} label="Logout" className='logout-btn'>
+            Logout
+          </button>
         )}
       </div>
     </nav>
